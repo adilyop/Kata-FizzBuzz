8bb38b9bcb94e261e61ce9df1292484b4af58003
Adil GUEZZI <aguezzi@Adils-MacBook-Pro.local>
2017-12-20 10:10:24 +0100

    code refactore

--- app/fizzBuzz.js
+++ app/fizzBuzz.js
@@ -1,34 +1,21 @@
-
-var config = require('../config/config');
-
-function fizzBuzzV0(input) {
-    let result = '';
-    if (isNaN(input) || input === '' || input === 0)
-        result = 'Error: Invalid Args';
-    else {
-        if (input % 3 === 0)
-            result = result + 'Fizz';
-        if (input % 5 === 0)
-            result = result + 'Buzz';
-        if (result === '')
-            result = input;
-    }
-    return result
-}
 function fizzBuzz(input) {
     let result = '';
-    if (isNaN(input) || input === '' || input === 0)
-        result = 'Error: Invalid Args';
-    else {
-        for (let i = 0; i < Object.keys(config).length; i += 1) {
-            if (input % Object.keys(config)[i] === 0)
-                result = result + config[Object.keys(config)[i]];
-        }
-        if (result === '')
-            result = input;
+    if (isNaN(input) || input === '') {
+        return 'Error';
     }
-    return result
+    if (input < 1 || input > 100) {
+        return 'Error';
+    }
+    if (input % 3 === 0) {
+        result += 'Fizz';
+    }
+    if (input % 5 === 0) {
+        result += 'Buzz';
+    }
+    if (result === '') {
+        result = input;
+    }
+    return result;
 }
-
 
 module.exports = fizzBuzz;
--- test/fizzBuzzTest.js
+++ test/fizzBuzzTest.js
@@ -4,34 +4,38 @@ const expect = chai.expect;
 const fizzBuzz = require('../app/fizzBuzz');
 
 describe('FizzBuzz Kata Test: \n', () => {
-    it('should return Error', (done) => {
-        expect(fizzBuzz('')).to.equal('Error: Invalid Args');
-        expect(fizzBuzz('test')).to.equal('Error: Invalid Args');
-        expect(fizzBuzz(0)).to.equal('Error: Invalid Args');
-        done();
-    });
-    it('should return number', (done) => {
+    it('should return input when input is not multiple of 3 and 5', (done) => {
         expect(fizzBuzz(1)).to.equal(1);
-        expect(fizzBuzz(16)).to.equal(16);
-        expect(fizzBuzz(98)).to.equal(98);
+        expect(fizzBuzz(2)).to.equal(2);
+        expect(fizzBuzz(4)).to.equal(4);
         done();
     });
-    it('should return Fizz', (done) => {
+    it('should return Fizz when input is multiple of 3', (done) => {
         expect(fizzBuzz(3)).to.equal('Fizz');
-        expect(fizzBuzz(36)).to.equal('Fizz');
-        expect(fizzBuzz(63)).to.equal('Fizz');
+        expect(fizzBuzz(6)).to.equal('Fizz');
+        expect(fizzBuzz(9)).to.equal('Fizz');
         done();
     });
-    it('should return Buzz', (done) => {
+    it('should return Buzz when input is 5', (done) => {
         expect(fizzBuzz(5)).to.equal('Buzz');
         expect(fizzBuzz(10)).to.equal('Buzz');
-        expect(fizzBuzz(80)).to.equal('Buzz');
+        expect(fizzBuzz(20)).to.equal('Buzz');
         done();
     });
-    it('should return FizzBuzz', (done) => {
+    it('should return FizzBuzz when input is multiple of 3 and 5', (done) => {
         expect(fizzBuzz(15)).to.equal('FizzBuzz');
         expect(fizzBuzz(30)).to.equal('FizzBuzz');
         expect(fizzBuzz(45)).to.equal('FizzBuzz');
+        done();
+    });
+    it('should return error when input is not a Number', (done) => {
+        expect(fizzBuzz('')).to.equal('Error');
+        expect(fizzBuzz('string')).to.equal('Error');
+        done();
+    });
+    it('should return error when number is greater than 100 or less than 1', (done) => {
+        expect(fizzBuzz(0)).to.equal('Error');
+        expect(fizzBuzz(101)).to.equal('Error');
         done();
     });
 });
